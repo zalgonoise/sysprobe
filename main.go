336715d0fca83d48474ee34f2e459bc016d56b33
ZalgoNoise <zalgo.noise@gmail.com>
2021-01-23 22:24:37 +0100

    [v0.1] ~ godocs - main.go

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+// Sysprobe is a simple, fast and modular binary to collect metadata
+// from your devices (I am talking to you, old rooted smartphone from 2016
+// which I no longer use but refuse to throw away) and push it to a NoSQL
+// database in your network (for metrics, alerts, etc)
+//
+// The point of the project is to have a one-binary-for-all when it comes to
+// collecting heartbeats from devices, regardless of the complexity or the
+// amount of data you want to collect.
+//
+// Currently it collects boring metadata such as the device's battery status
+// and its current IP address, but it's also packing a super powerful
+// network scanner that will ping all hosts in a subnet, and figure out their
+// open ports in just a few seconds. It puts nmap to shame. Boo, nmap, boo.
+//
 package main
 
 import (
