22099c6ba1d12643f07ab4d895714f9ed537c8d0
ZalgoNoise <zalgo.noise@gmail.com>
2021-01-23 20:19:46 +0100

    [v0.1] ~ termux-battery-status fallback

--- battery/battery.go
+++ battery/battery.go
@@ -2,6 +2,7 @@ package battery
 
 import (
 	"bufio"
+	"encoding/json"
 	"os"
 	"strconv"
 	"strings"
@@ -21,6 +22,46 @@ type Battery struct {
 	} `json:"temp"`
 }
 
+// TermuxBattery struct serves as a fallback object in case the uevent
+// file is not accessible to the user (root privileges are required)
+// It is a slower approach and thus not being the primary way to get
+// battery metadata
+type TermuxBattery struct {
+	Health   string  `json:"health"`
+	Capacity int     `json:"percentage"`
+	Plugged  string  `json:"plugged"`
+	Status   string  `json:"status"`
+	Temp     float32 `json:"temperature"`
+	Current  int     `json:"current"`
+}
+
+// Get method for TermuxBattery objects will execute the
+// `termux-battery-status` command, and retrieve the slice of bytes
+// which are already a JSON object. The method will unmarshal the JSON
+// object into a TermuxBattery object
+func (tb *TermuxBattery) Get() (*TermuxBattery, bool) {
+
+	exec, err := utils.Run("termux-battery-status")
+
+	if err != nil {
+		json.Unmarshal(exec, tb)
+		return tb, true
+	} else {
+		return tb, false
+	}
+}
+
+// Push method for TermuxBattery will inject the values from the
+// TermuxBattery object into the Battery (message) object
+func (tb *TermuxBattery) Push(b *Battery) *Battery {
+	b.Capacity = tb.Capacity
+	b.Health = tb.Health
+	b.Status = tb.Status
+	b.Temp.Internal = tb.Temp
+
+	return b
+}
+
 // Get method - collects battery related values
 // from /sys/class/power_supply/*/uevent, and returns a
 // pointer to the Battery struct with this data
@@ -29,7 +70,17 @@ func (b *Battery) Get(batteryLoc string) *Battery {
 	batteryFile := "/sys/class/power_supply/" + batteryLoc + "/uevent"
 
 	bat, err := os.Open(batteryFile)
-	utils.Check(err)
+	if err != nil {
+		tb := &TermuxBattery{}
+		tb, success := tb.Get()
+
+		if success != true {
+			return b
+		}
+
+		tb.Push(b)
+		return b
+	}
 	defer bat.Close()
 
 	scanner := bufio.NewScanner(bat)
